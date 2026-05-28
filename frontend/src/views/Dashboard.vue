12a6930e8dae1b7553d031627a0226cccf3513a1
little4-creator <xiayuqiang.xya@alibaba-inc.com>
2026-05-28 20:57:11 +0800

    ui: move Browser column before Target

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -184,6 +184,7 @@ function statusType(s: string | null) {
         </template>
       </el-table-column>
       <el-table-column prop="user_email" label="User" width="200" show-overflow-tooltip />
+      <el-table-column prop="browser_type" label="Browser" width="100" />
       <el-table-column prop="target" label="Target" min-width="160" show-overflow-tooltip />
       <el-table-column label="Exit" width="70">
         <template #default="{ row }: { row: BuildRow }">
@@ -220,7 +221,6 @@ function statusType(s: string | null) {
       </el-table-column>
       <el-table-column prop="platform" label="Platform" width="120" />
       <el-table-column prop="build_type" label="Type" width="90" />
-      <el-table-column prop="browser_type" label="Browser" width="100" />
     </el-table>
 
     <el-pagination
